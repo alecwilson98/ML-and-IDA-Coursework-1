5075be9b5bb70c6127451eb6b744662dc41821c2
Alec Wilson <alecwilson98@gmail.com>
2020-11-16 12:11:54 +0000

    Adjusted hyperparameter values.

--- model.py
+++ model.py
@@ -21,55 +21,47 @@ y_test = dft['label']
 
 class Classifiers():
     def rfc(self):
-        rf = RandomForestClassifier(random_state=1)
-        n_estimators = [10, 100, 1000]
-        max_features = [range(1, 21, 2)]
-        max_depth = [10, 15, 20]
+        rf = RandomForestClassifier()
+        n_estimators = [10, 100, 1000, 1200, 1500]
+        max_features = [17, 21, 25]
+        max_depth = [15, 20, 25]
         hyper_rf = dict(n_estimators=n_estimators, max_depth=max_depth, max_features=max_features)
         grid_rf = GridSearchCV(rf, hyper_rf, scoring='accuracy', n_jobs=-1)
-        grid_rf.fit(X_train, y_train)
+        grid_rf_fit = grid_rf.fit(X_train, y_train)
 
         y_pred = grid_rf.predict(X_test)
         print(metrics.classification_report(y_test, y_pred))
-        print('Best accuracy score: %s ' % y_pred.best_score_)
-        print('Best Hyperparameters: %s' % y_pred.best_params_)
+        print('Best accuracy score: %s ' % grid_rf_fit.best_score_)
+        print('Best Hyperparameters: %s' % grid_rf_fit.best_params_)
 
         joblib.dump(grid_rf.best_estimator_, 'rf_results.pkl', compress=1)
     def svm(self):
-        sv = SVC(random_state=2)
-        kernal = ['linear', 'poly', 'rbf', 'sigmoid']
+        sv = SVC()
+        kernel = ['linear', 'poly', 'rbf', 'sigmoid']
         C = [100, 10, 1, 0.1, 0.01]
-        hyper_sv = dict(kernal=kernal, C=C)
+        hyper_sv = dict(kernel=kernel, C=C)
         grid_sv = GridSearchCV(sv, hyper_sv, scoring='accuracy', n_jobs=-1)
-        grid_sv.fit(X_train, y_train)
+        grid_sv_fit = grid_sv.fit(X_train, y_train)
 
         y_pred2 = grid_sv.predict(X_test)
         print(metrics.classification_report(y_test, y_pred2))
-        print('Best accuracy score: %s ' % y_pred2.best_score_)
-        print('Best Hyperparameters: %s' % y_pred2.best_params_)
-
-        cm_2 = confusion_matrix(y_test, y_pred2)
-        cm_2.plot()
-        plt.show()
+        print('Best accuracy score: %s ' % grid_sv_fit.best_score_)
+        print('Best Hyperparameters: %s' % grid_sv_fit.best_params_)
 
         joblib.dump(grid_sv.best_estimator_, 'sv_results.pkl', compress=1)
     def knn(self):
-        kn = KNeighborsClassifier(random_state=3)
-        n_neighbors = range(1, 21, 2)
+        kn = KNeighborsClassifier()
+        n_neighbors = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21]
         weights = ['uniform', 'distance']
         metric = ['euclidean', 'manhattan', 'minkowski']
         hyper_kn = dict(n_neighbors=n_neighbors, weights=weights, metric=metric)
         grid_kn = GridSearchCV(kn, hyper_kn, scoring='accuracy', n_jobs=-1)
-        grid_kn.fit(X_train, y_train)
+        grid_kn_fit = grid_kn.fit(X_train, y_train)
 
         y_pred3 = grid_kn.predict(X_test)
         print(metrics.classification_report(y_test, y_pred3))
-        print('Best accuracy score: %s ' % y_pred3.best_score_)
-        print('Best Hyperparameters: %s' % y_pred3.best_params_)
-
-        cm_3 = confusion_matrix(y_test, y_pred3)
-        cm_3.plot()
-        plt.show()
+        print('Best accuracy score: %s ' % grid_kn_fit.best_score_)
+        print('Best Hyperparameters: %s' % grid_kn_fit.best_params_)
 
         joblib.dump(grid_kn.best_estimator_, 'kn_results.pkl', compress=1)
 def main():
